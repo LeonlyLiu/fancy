a0c2acd95f06d1a38f7910bf43542c63203cfd39
LeonlyLiu <leonly012@gmail.com>
2016-11-30 18:56:53 +0900

    Update Main.java

--- follow/Main.java
+++ follow/Main.java
@@ -1,4 +1,4 @@
-package followLine;
+package follow;
 
 import lejos.hardware.motor.Motor;
 import lejos.robotics.RegulatedMotor;
@@ -6,93 +6,72 @@ import lejos.utility.Delay;
 
 public class Main {
 
-	private static int timer = 0;
-	private static RegulatedMotor leftMotor =  Motor.C;
-	private static RegulatedMotor rightMotor = Motor.B;
+	int timer = 0;
+	RegulatedMotor leftMotor = Motor.C;
+	RegulatedMotor rightMotor = Motor.B;
 
-	public static void main(String[] args) {
+	public void main(String[] args) {
 
-		// 前進
-		while ( timer <= 1500){
-			forwardRobot( 700, 0);
-			Delay.msDelay(100);
-			timer += 100;
-		}
+		//前進L1
+		runStraight(1500, 700, 0);
 
 		//90°左に回転
-		timer = 0;
-		while ( timer <= 200){
-			turnLeft(380, 0);
-			Delay.msDelay(100);
-			timer += 100;
-		}
+		runTurn(200, 380, 0);
 
-		//前進
-		timer = 0;
-		while ( timer <= 2500){
-			forwardRobot(700, 0);
-			Delay.msDelay(100);
-			timer += 100;
-		}
+		//前進L2
+		runStraight(2500, 700, 0);
 
 		//45°左に回転
-		timer = 0;
-		while ( timer <= 100){
-			turnLeft(200, 0);
-			Delay.msDelay(100);
-			timer += 100;
-		}
+		runTurn(100, 200, 0);
 
 		//大きい曲進
-		timer = 0;
-		while ( timer <= 3000){
-			forwardRobot(300, 100);
-			Delay.msDelay(100);
-			timer += 100;
-		}
+		runStraight(3000, 300, 100);
 
-		//160°右に回転
-		timer = 0;
-		while ( timer <= 500){
-			turnRight(360, 0);
-			Delay.msDelay(100);
-			timer += 100;
-		}
+		//180°左に回転
+		runTurn(600, 360, 0);
 
 		//小さい曲進
-		timer = 0;
-		while ( timer <= 2000){
-			forwardRobot(300, 130);
-			Delay.msDelay(100);
-			timer += 100;
-		}
+		runStraight(2000, 300, 130);
 
 	}
 
+	//前進
+	public void runStraight(int time, int speed, int turn) {
+		timer = 0;
+		while(timer <= time) {
+			forwardRobot(speed, turn);
+			Delay.msDelay(100);
+			timer += 100;
+		}
+	}
+
+	//左に回転
+	public void runTurn(int time, int speed, int turn) {
+		timer = 0;
+		while ( timer <= time) {
+			turnLeft( speed, turn);
+			Delay.msDelay(100);
+			timer += 100;
+		}
+	}
+
 	//速度設定
-	private static void setSpeed(int speed, int turn) {
+	private void setSpeed(int speed, int turn) {
 		leftMotor.setSpeed(speed - turn);
 		rightMotor.setSpeed(speed + turn);
 	}
 
 	//ロボット前進
-	private static void forwardRobot(int speed, int turn) {
+	private void forwardRobot(int speed, int turn) {
 		setSpeed(speed, turn);
 		leftMotor.forward();
 		rightMotor.forward();
 	}
 
 	//左に回転
-	private static void turnLeft(int speed, int turn) {
+	private void turnLeft(int speed, int turn) {
 		setSpeed(speed, turn);
 		leftMotor.backward();
 		rightMotor.forward();
-	}
-
-	//右に回転
-	private static void turnRight(int speed, int turn) {
-		setSpeed(speed, turn);
-		leftMotor.forward();
-		rightMotor.backward();
 	}
 }
